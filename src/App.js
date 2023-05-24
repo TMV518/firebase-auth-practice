30a6bd8e9cde111b833c6c3e54ee24483b0d8d71
tmv518 <tmv810@gmail.com>
2023-05-24 13:48:22 -0400

    Finished project, added custom methods containing authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
   const [registerEmail, setRegisterEmail] = useState("");
@@ -7,33 +14,92 @@ function App() {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
 
-  const register = async () => {};
+  //user object
+  const [user, setUser] = useState({});
 
-  const login = async () => {};
+  useEffect(() => {
+    //similar to useEffect(), when auth changes, do callback function
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(auth.currentUser);
+    });
+  }, []);
 
-  const logout = async () => {};
+  //creates user account
+  //createUserWithEmailAndPassword(auth, email, password)
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //signs in
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //logs out
+  const logout = async () => {
+    await signOut(auth);
+  };
 
   return (
     <div className="App">
       <div>
         <h3> Register User </h3>
-        <input placeholder="Email..." />
-        <input placeholder="Password..." />
+        <input
+          placeholder="Email..."
+          onChange={(e) => {
+            setRegisterEmail(e.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(e) => {
+            setRegisterPassword(e.target.value);
+          }}
+        />
 
-        <button>Create User</button>
+        <button onClick={register}>Create User</button>
       </div>
 
       <div>
         <h3> Login </h3>
-        <input placeholder="Email..." />
-        <input placeholder="Password..." />
+        <input
+          placeholder="Email..."
+          onChange={(e) => {
+            setLoginEmail(e.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(e) => {
+            setLoginPassword(e.target.value);
+          }}
+        />
 
-        <button> Login</button>
+        <button onClick={login}> Login</button>
       </div>
 
       <h4> User Logged In: </h4>
+      {user?.email}
 
-      <button> Sign Out </button>
+      <button onClick={logout}> Sign Out </button>
     </div>
   );
 }
